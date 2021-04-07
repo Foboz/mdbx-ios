8374f2c277dd474a3816f45cab95d645114d2f38
Mikhail Nikanorov <fobozsb@gmail.com>
2021-04-07 16:39:43 -0700

    Package update

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
       name: "mdbx-ios",
       dependencies: ["mdbx-framework"]),
     .binaryTarget(name: "mdbx-framework",
-                  url: "https://github.com/Foboz/mdbx-ios/releases/download/1.0.0/mdbx_framework.xcframework.zip",
+                  url: "https://github.com/Foboz/mdbx-ios/releases/download/1.0.0/libmdbx_framework.xcframework.zip",
                   checksum: "46ce0391693cff2c6c8ba5399df9bc6414770bece8fdad6351635cae228ee5e8")
   ]
 )
