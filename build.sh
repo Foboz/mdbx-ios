de01a4ad4ba835b6379d679de958876526cfb35c
Misha Nikanorov <fobozsb@gmail.com>
2021-06-19 01:10:13 -0700

    feature/dependency_update: Move out build folder from mdbx

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ if ! which cmake >/dev/null ; then
 fi
 
 # Make with ios-toolchain
-MAKE_SCRIPT="cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../../ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_BITCODE=ON"
+MAKE_SCRIPT="cmake ../libmdbx -G Xcode -DCMAKE_TOOLCHAIN_FILE=../../ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_BITCODE=ON"
 
-$(cd libmdbx/libmdbx && rm -rf build)
-$(cd libmdbx/libmdbx && mkdir build)
-$(cd libmdbx/libmdbx/build && ${MAKE_SCRIPT})
+$(cd libmdbx && rm -rf build)
+$(cd libmdbx && mkdir build)
+$(cd libmdbx/build && ${MAKE_SCRIPT})
